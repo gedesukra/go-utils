a84a2566f4e4cfa59ffc68ac1cda09afb3671cda
GedeSukra Bawe <gedesukrabawe@gmail.com>
2020-09-19 11:33:42 +0700

    change first letter to Uppercase

--- util_error.go
+++ util_error.go
@@ -9,7 +9,7 @@ var ConsUnableReadBody string = "Unable read http body"
 var ConsUnableUnmarshal string = "Unable unmarshal json"
 var ConsUnableMarshal string = "Unable marshal obj"
 
-func getStacktraceError(err error) string {
+func GetStacktraceError(err error) string {
 	pc, fn, line, _ := runtime.Caller(1)
 	return fmt.Sprintf("stacktrace -> in %s[%s:%d] %v", runtime.FuncForPC(pc).Name(), fn, line, err)
 }
